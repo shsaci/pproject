c61ef243fbdee5815204e5118437ac89ee70c1e3
shsaci <shsaci@yahoo.com.au>
2016-08-11 11:35:47 +1200

    Initial handlebars - working placement

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const testObj = {
   logoImg: "http://placekitten.com/g/680/190",
   des1: 'Description: Lorem ipsum dolor sit amet',
   des2: 'Great Britain Formula1.com',
-  blurb: 'My first Blurb'
+  blurb: 'My first Blurb is this. Its a very long blurb simply because its my first blurb. That is just the way I am. You got it!!'
 }
 
 function get (req, res) {
