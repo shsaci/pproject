785dd7815e69840eda04225c5844e9332c54ccad
shsaci <shsaci@yahoo.com.au>
2016-08-11 17:30:30 +1200

    adding entity table

---
+++ seeds/entity.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('entity').del()
+  .then(function () {
+    return Promise.all([
+      // Inserts seed entries
+      knex('entity').insert({id: 1, logoImg: 'http://placekitten.com/g/680/190', des1: 'Description: Lorem ipsum dolor sit amet',
+      des2: 'Great Britain Formula1.com', entityID: 'form1'})
+    ]);
+  });
+};
